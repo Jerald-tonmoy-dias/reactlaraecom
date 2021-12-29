8d4a7f4541607c65dbbadf931c66df44a5dca632
Jerald Tonmoy Dias <tonmoyjareld@gmail.com>
2021-12-29 23:31:07 +0600

    public route and collection

--- client/src/components/frontend/Collection.js
+++ client/src/components/frontend/Collection.js
@@ -3,7 +3,18 @@ import React from "react";
 export default function Collection() {
   return (
     <>
-      <h1>Collections</h1>
+      <div className="container mt-5">
+        <nav aria-label="breadcrumb" className="bg-warning py-3 px-4">
+          <ol class="breadcrumb">
+            <li
+              class="breadcrumb-item active text-white display-5"
+              aria-current="page"
+            >
+              Home
+            </li>
+          </ol>
+        </nav>
+      </div>
     </>
   );
 }
